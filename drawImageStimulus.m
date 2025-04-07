f4343e7ae8ff3ac919a4a8111f98f597ee98cd6e
NeuroMoon <mjm2405@gmail.com>
2025-04-07 17:50:26 -0500

    update

--- drawImageStimulus.m
+++ drawImageStimulus.m
@@ -5,11 +5,7 @@ function drawImageStimulus(p)
 % 2018-05-15  YB   wrote it. <yoonbai@utexas.edu>
 %
 
-modName                 = p.trial.modName;
-current_image_idx       = p.trial.(modName).states.current_img_index;
-current_frame_idx= ((current_image_idx*p.trial.(modName).NUM_FRAME)-p.trial.(modName).NUM_FRAME)+  p.trial.(modName).states.current_frame_index;
-% when using 'generateImageStimuliForSession.m'
-texturePtrIndex         = current_frame_idx;%p.trial.(modName).imageList{p.trial.(modName).sessionTrialIndex}(current_image_idx);
+texturePtrIndex         = p.trial.(modName).videoList(p.trial.(modName).sessionTrialIndex);
 
 Screen('DrawTextures', p.trial.display.ptr, ...
     p.trial.(modName).stimulus.image.pointers(texturePtrIndex), ...
